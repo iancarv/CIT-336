b4da8c457dd9c2affdf454df8d97582fe170e98e
Ian Carvalho <iancarv@gmail.com>
2016-04-12 16:21:39 -0400

    RoR Blog Homework

---
+++ Midterm/Site/flavors.php
@@ -0,0 +1,29 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "root", "Midterm_CIan");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt select query execution
+$sql = "SELECT * FROM Flavors";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_array($result)){
+            echo "<option value=" . $row['ID'] . ">" . $row['Name'] . "</option>";
+        }
+        // Close result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
